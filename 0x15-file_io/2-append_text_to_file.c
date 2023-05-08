fbe313f34c2cf1872ac16615d7698fa15deecb28
Amiine21 <amine.labdaoui@gmail.com>
2023-05-08 08:23:37 -0700

    Update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,26 +1,34 @@
 #include "main.h"
 
 /**
-    * append_text_to_file - append text to the end of a file
-     * @filename: name of file to write into.
-      * @text_content: text to append to file
-       * Return: 1 (SUCCESS), else -1 (FAILURE)
-	*/
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: A pointer to the name of the file.
+ * @text_content: The string to add to the end of the file.
+ *
+ * Return: If the function fails or filename is NULL - -1.
+ *         If the file does not exist the user lacks write permissions - -1.
+ *         Otherwise - 1.
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, l = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content != NULL)
 	{
-		for (l = 0; text_content[l];)
-			l++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
+
 	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, l);
+	w = write(o, text_content, len);
+
 	if (o == -1 || w == -1)
 		return (-1);
+
 	close(o);
+
 	return (1);
 }
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,15 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *read_file(char *file);
+char *create_buffer(char *file);
 void close_file(int fd);
 
 /**
- *read_file - function that read a file into a buffer
- *@file: file to read form
- *Return: A pointer to the newly-allocated buffer.
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ *
+ * Return: A pointer to the newly-allocated buffer.
  */
-char *read_file(char *file)
+char *create_buffer(char *file)
 {
 	char *buffer;
 
@@ -18,12 +19,14 @@ char *read_file(char *file)
 
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
 	return (buffer);
 }
+
 /**
  * close_file - Closes file descriptors.
  * @fd: The file descriptor to be closed.
@@ -43,30 +46,37 @@ void close_file(int fd)
 
 /**
  * main - Copies the contents of a file to another file.
- * @ac: argument count.
- * @av: argoument values.
- * Return: 0 (success)
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
  */
-int main(int ac, char *av[])
+int main(int argc, char *argv[])
 {
 	int from, to, r, w;
 	char *buffer;
 
-	if (ac != 3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	buffer = read_file(av[2]);
-	from = open(av[1], O_RDONLY);
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
 	r = read(from, buffer, 1024);
-	to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
 		if (from == -1 || r == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
@@ -74,13 +84,14 @@ int main(int ac, char *av[])
 		w = write(to, buffer, r);
 		if (to == -1 || w == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
 
 		r = read(from, buffer, 1024);
-		to = open(av[2], O_WRONLY | O_APPEND);
+		to = open(argv[2], O_WRONLY | O_APPEND);
 
 	} while (r > 0);
 
